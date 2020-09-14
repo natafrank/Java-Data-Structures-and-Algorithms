5e87e281826124fad5fcd2247b1df414b023c62f
Francisco Ortiz <francisco.ortiz@oracle.com>
2020-09-14 15:27:38 -0500

    first commit

---
+++ src/main/java/com/packt/datastructuresandalg/lesson1/activity/octaltodecimal/OctalToDecimal.java
@@ -0,0 +1,9 @@
+package com.packt.datastructuresandalg.lesson1.activity.octaltodecimal;
+
+public class OctalToDecimal {
+    public int convertToDecimal(String octal) {
+        return 999;
+    }
+
+    //public static void main String
+}
