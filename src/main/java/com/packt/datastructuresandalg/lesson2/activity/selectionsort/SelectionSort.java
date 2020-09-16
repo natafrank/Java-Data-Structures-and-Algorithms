63d7c35bd71e75f16d74b878c43fa1f50a951057
Francisco Ortiz <francisco.ortiz@oracle.com>
2020-09-16 17:48:29 -0500

    Adds selection sort actvity

--- src/main/java/com/packt/datastructuresandalg/lesson2/activity/selectionsort/SelectionSort.java
+++ src/main/java/com/packt/datastructuresandalg/lesson2/activity/selectionsort/SelectionSort.java
@@ -3,6 +3,20 @@ package com.packt.datastructuresandalg.lesson2.activity.selectionsort;
 public class SelectionSort {
 
     public void sort(int[] array) {
+        for(int i = 0; i < array.length - 1; i++){
+            int indexOfMin= i;
+            for(int j = i; j < array.length; j++){
+                if(array[j] < array[indexOfMin]){
+                    indexOfMin = j;
+                }
+            }
+            swap(array, indexOfMin, i);
+        }
+    }
 
+    private void swap(int[] numbers, int j, int k) {
+        int temp = numbers[j];
+        numbers[j] = numbers[k];
+        numbers[k] = temp;
     }
 }
