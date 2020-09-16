ed643687da8a7f7b900c3dbbd89d6fd3aca928b6
Francisco Ortiz <francisco.ortiz@oracle.com>
2020-09-16 15:14:09 -0500

    Finiches Intersection and Octal problem

--- src/main/java/com/packt/datastructuresandalg/lesson1/activity/improveintersection/Intersection.java
+++ src/main/java/com/packt/datastructuresandalg/lesson1/activity/improveintersection/Intersection.java
@@ -2,9 +2,7 @@ package com.packt.datastructuresandalg.lesson1.activity.improveintersection;
 
 import com.packt.datastructuresandalg.lesson1.binarysearch.BinarySearch;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Intersection {
 
@@ -21,7 +19,28 @@ public class Intersection {
     }
 
     public List<Integer> intersectionFast(int[] a, int[] b) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        mergeSort(a);
+        mergeSort(b);
+
+        int indexA = 0;
+        int indexB = 0;
+
+        while(indexA < a.length && indexB < b.length){
+            if(a[indexA] > b[indexB]){
+                indexB++;
+            }
+            else if(b[indexB] > a[indexA]){
+                indexA++;
+            }
+            else{
+                result.add(a[indexA]);
+                indexA++;
+                indexB++;
+            }
+        }
+
+        return result;
     }
 
     public void mergeSort(int[] input) {
@@ -30,7 +49,7 @@ public class Intersection {
 
     public static void main(String[] args) {
         Intersection inter = new Intersection();
-        System.out.println(inter.intersection(new int[]{4, 7, 5, 2, 3}, new int[]{4, 2, 3, 9, 1}));
+        System.out.println(inter.intersection(new int[]{4, 7, 5, 2, 3}, new int[]{4, 2, 3, 9, 1}));// a = {2, 3, 4, 5, 7*}   b = {1, 2, 3, 4, 9*}   result{2, 3, 4}
         System.out.println(inter.intersection(new int[]{4, 6, 11, 2, 3}, new int[]{5, 11, 3, 9, 1}));
 
         System.out.println(inter.intersectionFast(new int[]{4, 7, 5, 2, 3}, new int[]{4, 2, 3, 9, 1}));
--- src/main/java/com/packt/datastructuresandalg/lesson1/activity/octaltodecimal/OctalToDecimal.java
+++ src/main/java/com/packt/datastructuresandalg/lesson1/activity/octaltodecimal/OctalToDecimal.java
@@ -2,8 +2,17 @@ package com.packt.datastructuresandalg.lesson1.activity.octaltodecimal;
 
 public class OctalToDecimal {
     public int convertToDecimal(String octal) {
-        return 999;
+        int result = 0;
+        int currentPow = 1;
+        for(int i = octal.length() - 1; i >= 0; i--){
+            result += getNumberFromStringAtPosition(octal, i) * currentPow;
+            currentPow *= 8;
+        }
+
+        return result;
     }
 
-    //public static void main String
+    private int getNumberFromStringAtPosition(String string, int position){
+        return Integer.parseInt(string.charAt(position) + "");
+    }
 }
